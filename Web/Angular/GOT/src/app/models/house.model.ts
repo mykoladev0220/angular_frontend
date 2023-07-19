e4265808484cbd11a5a4241346ff4ae591c5e0bf
mykoladev0220 <mykolademchukdev@gmail.com>
2023-07-19 15:22:20 +0200

    removed comments

--- Web/Angular/GOT/src/app/models/house.model.ts
+++ Web/Angular/GOT/src/app/models/house.model.ts
@@ -4,19 +4,19 @@ export interface House {
     house : House,
     houses : House[],
     url: string,
-    name: string,//
-    region: string,//
-    coatOfArms: string,//
-    words: string,//
-    titles: string[],//
-    seats: string[],//
-    currentLord: string, //
-    heir: string,//
-    overlord: string,//
-    founded: string,//
-    founder: string, //
+    name: string,
+    region: string,
+    coatOfArms: string,
+    words: string,
+    titles: string[],
+    seats: string[],
+    currentLord: string, 
+    heir: string,
+    overlord: string,
+    founded: string,
+    founder: string, 
     diedOut: string,
-    ancestralWeapons: string[], //
-    cadetBranches: string[], //
-    swornMembers: string[] //
+    ancestralWeapons: string[],
+    cadetBranches: string[],
+    swornMembers: string[] 
 }
